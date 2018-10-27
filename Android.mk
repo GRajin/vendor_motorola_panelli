78d8318dfd8c6b479daaf01df1269f604ad42f33
LinuxPanda <LinuxPanda@users.noreply.github.com>
2018-10-27 16:45:18 +0000

    initial commit

---
+++ Android.mk
@@ -0,0 +1,54 @@
+#
+# Copyright (C) 2018 The LineageOS Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+LOCAL_PATH := $(call my-dir)
+
+ifneq ($(filter panelli, $(TARGET_DEVICE)),)
+
+include $(CLEAR_VARS)
+    LOCAL_SRC_FILES_32 = proprietary/vendor/lib/libged.so
+    LOCAL_MULTILIB = 32
+
+    LOCAL_MODULE = libged
+    LOCAL_MODULE_SUFFIX = .so
+
+    LOCAL_PROPRIETARY_MODULE := true
+    LOCAL_MODULE_CLASS = SHARED_LIBRARIES
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+    LOCAL_SRC_FILES_32 = proprietary/vendor/lib/libion_mtk.so
+    LOCAL_MULTILIB = 32
+
+    LOCAL_MODULE = libion_mtk
+    LOCAL_MODULE_SUFFIX = .so
+
+    LOCAL_PROPRIETARY_MODULE := true
+    LOCAL_MODULE_CLASS = SHARED_LIBRARIES
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+    LOCAL_SRC_FILES_32 = proprietary/vendor/lib/libratconfig.so
+    LOCAL_MULTILIB = 32
+
+    LOCAL_MODULE = libratconfig
+    LOCAL_MODULE_SUFFIX = .so
+
+    LOCAL_PROPRIETARY_MODULE := true
+    LOCAL_MODULE_CLASS = SHARED_LIBRARIES
+include $(BUILD_PREBUILT)
+
+endif
