64f6c566f998853fab9e272a308ce314f1857033
LinuxPanda <LinuxPanda@users.noreply.github.com>
2018-11-09 02:15:59 +0530

    update libs path & export libion_mtk to build system graphics libs from source

--- Android.mk
+++ Android.mk
@@ -19,6 +19,17 @@ LOCAL_PATH := $(call my-dir)
 ifneq ($(filter panelli, $(TARGET_DEVICE)),)
 
 include $(CLEAR_VARS)
+    LOCAL_SRC_FILES_32 = vendor/lib/libion_mtk.so
+    LOCAL_MULTILIB = 32
+
+    LOCAL_MODULE = libion_mtk
+    LOCAL_MODULE_SUFFIX = .so
+
+    LOCAL_PROPRIETARY_MODULE := true
+    LOCAL_MODULE_CLASS = SHARED_LIBRARIES
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
     LOCAL_SRC_FILES_32 = vendor/lib/libratconfig.so
     LOCAL_MULTILIB = 32
 
